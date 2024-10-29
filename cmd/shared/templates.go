3b0295a799ea713a90176e36da4fd7b79177d82b
Arthur Polimeni <arthurpolimeni17@gmail.com>
2024-10-28 21:19:52 -0300

    add module: kuma-changelog-generator

--- cmd/shared/templates.go
+++ cmd/shared/templates.go
@@ -3,17 +3,17 @@ package shared
 import "github.com/arthurbcp/kuma/v2/internal/domain"
 
 var Templates = map[string]domain.Template{
-	"arthurbcp/typescript-rest-openapi-services": domain.NewTemplate(
+	"kuma-framework/kuma-typescript-rest-services": domain.NewTemplate(
 		"TypeScript Rest Services (OpenAPI 2.0)",
 		"Create a library TypeScript with services typed for all endpoints described in a file Open API 2.0",
 		[]string{"typescript", "openapi", "rest", "library"},
 	),
-	"arthurbcp/kuma-hello-world": domain.NewTemplate(
+	"kuma-framework/kuma-hello-world": domain.NewTemplate(
 		"Hello World",
 		"A simple Hello World in Go!",
 		[]string{"golang", "example"},
 	),
-	"arthurbcp/kuma-changelog-generator": domain.NewTemplate(
+	"kuma-framework/kuma-changelog-generator": domain.NewTemplate(
 		"Changelog Generator",
 		"Helper to write a good changelog to your project",
 		[]string{"changelog", "helper", "markdown"},
